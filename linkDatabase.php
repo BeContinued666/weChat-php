0050188f66da207dc97430d0338f44262e88df0a
lgz <2809846026@qq.com>
2019-11-18 15:16:46 +0800

    第一次提交

---
+++ linkDatabase.php
@@ -0,0 +1,14 @@
+<?php
+// 引入数据库配置文件
+include_once 'config.php';
+
+// 创建数据库连接
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+// 检查连接是否成功
+if(!$con) {
+    die('数据库连接失败xxxx');
+}
+
+// 关闭数据库连接
+// mysql_close($com);
