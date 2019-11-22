3f6ac64b168dddeb856515291218a7c6b59fd807
lgz <2809846026@qq.com>
2019-11-22 15:29:47 +0800

    验证成功

--- config.php
+++ config.php
@@ -1,20 +1,58 @@
-
 <?php
-// 配置公共参数
+namespace LaneWeChat;
+/**
+ * 系统主配置文件.
+ */
+//版本号
+define('LANEWECHAT_VERSION', '1.4');
+define('LANEWECHAT_VERSION_DATE', '2014-11-05');
 
+/*
+ * 服务器配置，详情请参考@link http://mp.weixin.qq.com/wiki/index.php?title=接入指南
+ */
+define("WECHAT_URL", 'http://39.100.77.213/');
+define('WECHAT_TOKEN', 'xier618');
+define('ENCODING_AES_KEY', "MqAuKoex6FyT5No0OcpRyCicThGs0P1vz4mJ2gwvvkF");
 
-// 数据库参数---------------------------------
-// 数据库连接地址
-$db_host = 'bdm72186670.my3w.com';
+/*
+ * 开发者配置
+ */
+define("WECHAT_APPID", 'wxc4445416897aca36');
+define("WECHAT_APPSECRET", '09429e7971bf307f5123ccf7913d3847');
 
-// 数据库名
-$db_name = 'bdm72186670_db';
+/*
+ * SAE平台配置
+ */
+define("HTTP_ACCESSKEY", '04xmzo3zm5');
+define("HTTP_APPNAME", 'imcustom4test');
 
-// 数据库用户名
-$db_user = 'bdm72186670';
-
-// 数据库用户密码
-$db_pass = 'Lu646354561';
-
-// 表前缀
-$table_prefix = 'wx_';
+////-----引入系统所需类库-------------------
+////引入错误消息类
+//include_once 'core/msg.lib.php';
+////引入错误码类
+//include_once 'core/msgconstant.lib.php';
+////引入CURL类
+//include_once 'core/curl.lib.php';
+//
+////-----------引入微信所需的基本类库----------------
+////引入微信处理中心类
+//include_once 'core/wechat.lib.php';
+////引入微信请求处理类
+//include_once 'core/wechatrequest.lib.php';
+////引入微信被动响应处理类
+//include_once 'core/responsepassive.lib.php';
+////引入微信access_token类
+//include 'core/accesstoken.lib.php';
+//
+////-----如果是认证服务号，需要引入以下类--------------
+////引入微信权限管理类
+//include_once 'core/wechatoauth.lib.php';
+////引入微信用户/用户组管理类
+//include_once 'core/usermanage.lib.php';
+////引入微信主动相应处理类
+//include_once 'core/responseinitiative.lib.php';
+////引入多媒体管理类
+//include_once 'core/media.lib.php';
+////引入自定义菜单类
+//include_once 'core/menu.lib.php';
+?>
